cafd80f96792854a44421f88267deec175f1069a
pavansk06 <pxkosuru@zonemail.clpccd.edu>
2024-07-03 00:24:41 -0700

    commit 1

--- main.py
+++ main.py
@@ -7,6 +7,10 @@ def main():
     Code Your Program here
     ########################################
     """
+    i = 0
+    while i < 5:
+        i = i + 1
+        numbers.append(int.random())
 
     print(f'The random values are {numbers}')
     print(f'The total is {total}')
